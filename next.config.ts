a7bfc002b7f6f1b6da980418f44606cda9e031e3
nsasa1971 <nsasa1971@gmail.com>
2026-04-25 23:33:20 +0200

    Restore next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Isključi Turbopack za build
+  turbopack: {
+    build: false,
+  },
 };
 
 export default nextConfig;
